9bf2062e68190f199e9b086f08a8ae20f49c5958
cpiker <cpiker@users.noreply.github.com>
2020-03-15 15:44:58 -0500

    Added docs for das2c_convert()

--- src/das2c_data.c
+++ src/das2c_data.c
@@ -1,11 +1,3 @@
-/*
-;+
-; OPTIONAL INPUTS: 
-;  slice:    Gets a sub-set of the data values.  By default the output
-;            array will have the same rank and extents as the overall
-;            dataset.  Thus all data appears to be scatter data.
-;-
-
 /* Copyright (C) 2020 Chris Piker <chris-piker@uiowa.edu>
  *                    
  * This file is part of das2dlm, an Interactive Data Language (IDL) binding
@@ -131,13 +123,42 @@
 ;
 ;  Get all unique frequency values for a rank
 ;
-;
 ; MODIFICATION HISTORY:
-;  Written by: Chris Piker, 2020-03-14
+;  In progress
 ;-
 */
 static IDL_VPTR das2c_api_data(int argc, IDL_VPTR* argv)
 {
+	/* Get the variable or quit */
 	
+	/*ptrdiff_t aDsShape[DASIDX_MAX] = DASIDX_INIT_UNUSED;
+	int nDsRank = 0;
+	const DasVar* pVar = NULL;
+	pVar = das2c_arg_to_var(argc, argv, 0, NULL, NULL, &nDsRank, aDsShape); * /
+	
+	*/
+	
+	/* Simplistic Algorithm:  
+	
+	   1. Get total extent requested
+		
+		2. Is it Zero items?   -yes-> return null
+		       |
+		       V
+		2. Do I have an array                -no-> make temp array
+		       | yes
+				 V
+		3. Is the extent equal to the array  -no-> make temp arary
+		       | yes
+				 V
+		4. Wrap the array either the permanent or temporary array
+	*/
+	
+	/*int aExtents[IDL_MAX_ARRAY_DIM]; */
+	
+	
+	/* Just wrap the basic array now as a test */
+	
+		
 	return IDL_GettmpNULL();
 }
