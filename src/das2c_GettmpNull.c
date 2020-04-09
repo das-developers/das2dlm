c486258432eb3a6d33bc4dc54c26cb6bb4b172e6
Chris Piker <cpiker@users.noreply.github.com>
2020-04-08 23:29:13 -0500

    Simple guess an undocumented flag

--- src/das2c_GettmpNull.c
+++ src/das2c_GettmpNull.c
@@ -1,6 +1,10 @@
 // IDL 8.4 does not have IDL_GettmpNULL() implementation. This is an attempt to implement it
 #if IDL_VERSION_MINOR < 7
 
+#ifndef IDL_V_NULL
+#define IDL_V_NULL          64	 /* Variable is !NULL */
+#endif
+
 IDL_VPTR IDL_GettmpNULL()
 {
 	IDL_VPTR tmp = IDL_Gettmp();
